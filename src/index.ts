1353f17298cb8218c587ace0b4cfd2c9a6d6140a
zaryab <zaryabbkh@gmail.com>
2025-09-16 08:23:45 +0500

    invoice print preview

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmr
 import { dashboardMeta } from './dashboard.meta';
 
 // Dashboard and Navigation Components
+import { createDashboardGroup } from './app-navigation/nav-utils';
 import { createLeftPanelLink } from './left-panel-link.component';
 import RootComponent from './root.component';
 
-// Billing Admin Components
-// import appMenu from './billable-services/billable-services-menu-item/item.component';
-// import BillableServiceHome from './billable-services/billable-services-home.component';
 import BillableServicesCardLink from './billable-services-admin-card-link.component';
 
 // Billing Core Components
@@ -32,6 +30,11 @@ import { EditBillForm } from './billable-services/bill-manager/workspaces/edit-b
 import { WaiveBillForm } from './billable-services/bill-manager/workspaces/waive-bill/waive-bill-form.workspace';
 import CreateBillWorkspace from './billable-services/bill-manager/workspaces/create-bill/create-bill.workspace';
 
+// Billable Services Components
+import CommodityForm from './billable-services/billables/commodity/commodity-form.workspace';
+import AddServiceForm from './billable-services/billables/services/service-form.workspace';
+import { BulkImportBillableServices } from './billable-services/bulk-import-billable-service.modal';
+
 // Order Components
 import DrugOrder from './billable-services/billiable-item/drug-order/drug-order.component';
 import ImagingOrder from './billable-services/billiable-item/test-order/imaging-order.component';
@@ -40,36 +43,29 @@ import PriceInfoOrder from './billable-services/billiable-item/test-order/price-
 import ProcedureOrder from './billable-services/billiable-item/test-order/procedure-order.component';
 import OrderActionButton from './billable-services/billiable-item/order-actions/components/order-action-button.component';
 
-// Billable Services Components
-import CommodityForm from './billable-services/billables/commodity/commodity-form.workspace';
-import AddServiceForm from './billable-services/billables/services/service-form.workspace';
-import { BulkImportBillableServices } from './billable-services/bulk-import-billable-service.modal';
-
 // Payment Points Components
 import { CreatePaymentPoint } from './payment-points/create-payment-point.component';
 import { ClockIn } from './payment-points/payment-point/clock-in.modal';
 import { ClockOut } from './payment-points/payment-point/clock-out.modal';
 
-// import ServiceMetrics from './billable-services/dashboard/service-metrics.component';
-
-import { createDashboardGroup } from './app-navigation/nav-utils';
-import { Money } from '@carbon/react/icons';
+// Bill Deposit Components
 import BillDepositSearch from './bill-deposit/components/search/bill-deposit-search.component';
 import AddDepositWorkspace from './bill-deposit/components/forms/add-deposit.workspace';
 import DeleteDepositModal from './bill-deposit/components/modal/delete-deposit.modal';
 import DepositTransactionWorkspace from './bill-deposit/components/forms/deposit-transactions/deposit-transaction.workspace';
 import ReverseTransactionModal from './bill-deposit/components/modal/reverse-transaction.modal';
 
-// import InitiatePaymentDialog from './invoice/payments/initiate-payment/initiate-payment.component';
+
 import VisitAttributeTags from './invoice/payments/visit-tags/visit-attribute.component';
 import DeletePaymentModeModal from './payment-modes/delete-payment-mode.modal';
 import PaymentModeWorkspace from './payment-modes/payment-mode.workspace';
 import RequirePaymentModal from './prompt-payment/prompt-payment-modal.component';
 
 // Print Preview Components
-// import PrintPreviewModal from './print-preview/print-preview.modal';
+import PrintPreviewModal from './print-preview/print-preview.modal';
 import PaymentWorkspace from './invoice/payments/payment-form/payment.workspace';
 
+// Translation
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 const moduleName = '@openmrs/esm-billing-app';
@@ -191,18 +187,6 @@ export const imagingOrder = getSyncLifecycle(ImagingOrder, options);
 export const drugOrder = getSyncLifecycle(DrugOrder, options);
 export const orderActionButton = getSyncLifecycle(OrderActionButton, options);
 
-// Bill Deposit Components
-export const billDepositSearch = getSyncLifecycle(BillDepositSearch, options);
-export const addDepositWorkspace = getSyncLifecycle(AddDepositWorkspace, options);
-export const deleteDepositModal = getSyncLifecycle(DeleteDepositModal, options);
-export const depositTransactionWorkspace = getSyncLifecycle(DepositTransactionWorkspace, options);
-export const reverseTransactionModal = getSyncLifecycle(ReverseTransactionModal, options);
-
-// Payment Points Components
-export const createPaymentPoint = getSyncLifecycle(CreatePaymentPoint, options);
-export const clockIn = getSyncLifecycle(ClockIn, options);
-export const clockOut = getSyncLifecycle(ClockOut, options);
-
 // Payment Components
 export const requirePaymentModal = getSyncLifecycle(RequirePaymentModal, options);
 export const visitAttributeTags = getSyncLifecycle(VisitAttributeTags, options);
@@ -211,10 +195,25 @@ export const paymentModeWorkspace = getSyncLifecycle(PaymentModeWorkspace, optio
 export const deletePaymentModeModal = getSyncLifecycle(DeletePaymentModeModal, options);
 export const paymentWorkspace = getSyncLifecycle(PaymentWorkspace, options);
 
+// Payment Points Components
+export const createPaymentPoint = getSyncLifecycle(CreatePaymentPoint, options);
+export const clockIn = getSyncLifecycle(ClockIn, options);
+export const clockOut = getSyncLifecycle(ClockOut, options);
+
 // Service Management Components
 export const addServiceForm = getSyncLifecycle(AddServiceForm, options);
 export const addCommodityForm = getSyncLifecycle(CommodityForm, options);
 export const bulkImportBillableServicesModal = getSyncLifecycle(BulkImportBillableServices, options);
+
+// Bill Deposit Components
+export const billDepositSearch = getSyncLifecycle(BillDepositSearch, options);
+export const addDepositWorkspace = getSyncLifecycle(AddDepositWorkspace, options);
+export const deleteDepositModal = getSyncLifecycle(DeleteDepositModal, options);
+export const depositTransactionWorkspace = getSyncLifecycle(DepositTransactionWorkspace, options);
+export const reverseTransactionModal = getSyncLifecycle(ReverseTransactionModal, options);
+
+// Print Preview Components
+export const printPreviewModal = getSyncLifecycle(PrintPreviewModal, options);
 
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
